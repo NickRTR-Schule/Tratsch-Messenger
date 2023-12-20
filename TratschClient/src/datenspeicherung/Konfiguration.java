fc657550037bd07848af4beeea48da8e01e0453e
Nick <95316598+NickRTR@users.noreply.github.com>
2023-12-20 15:01:48 +0100

    add configuration

--- TratschClient/src/datenspeicherung/Konfiguration.java
+++ TratschClient/src/datenspeicherung/Konfiguration.java
@@ -1,5 +1,19 @@
 package datenspeicherung;
 
 public class Konfiguration {
+    private String host;
+    private int port;
 
+    public Konfiguration(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public String liesHost() {
+        return host;
+    }
+
+    public int liesPort() {
+        return port;
+    }
 }
