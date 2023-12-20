b33260063a523b733afdc685cbd8382b2cecb3f6
Leo <80842710+szxl777@users.noreply.github.com>
2023-12-20 15:08:03 +0100

    Login Fenster

--- TratschClient/src/benutzerschnittstelle/Benutzerschnittstelle.java
+++ TratschClient/src/benutzerschnittstelle/Benutzerschnittstelle.java
@@ -1,91 +1,128 @@
 package benutzerschnittstelle;
 
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.*;
+import javax.swing.JList;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
 
-public class Benutzerschnittstelle extends JFrame {
+public class Benutzerschnittstelle extends JFrame
+{
 
-	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JList IstAngemeldetBenutzer;
+	private JTextArea txtTextnachrichten;
+	private JLabel lblAn;
+	private JTextArea txtEingabeTextnachricht;
+	private JButton btnAnmelden;
+	private JButton btnAbmelden;
+	private JButton btnSenden;
+	private JButton btnLoeschen;
 	private JTextField txtEmpfaenger;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		try
-		{
+	public static void main(String[] args)
+	{
+		
+		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		}
-		catch (Exception e)
-		{
+		catch(Exception e) {
 			System.out.println("Error setting native LAF: " + e);
 		}
 		
-		EventQueue.invokeLater(() -> {
-            try {
-                Benutzerschnittstelle frame = new Benutzerschnittstelle();
-                frame.setVisible(true);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					Benutzerschnittstelle frame = new Benutzerschnittstelle();
+					frame.setVisible(true);
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 	/**
 	 * Create the frame.
 	 */
-	public Benutzerschnittstelle() {
+	public Benutzerschnittstelle()
+	{
+		setTitle("Tratsch");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 454, 395);
+		setBounds(100, 100, 522, 386);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JList lstAngemeldeteBenutzer = new JList();
-		lstAngemeldeteBenutzer.setBounds(6, 6, 173, 308);
-		contentPane.add(lstAngemeldeteBenutzer);
+		IstAngemeldetBenutzer = new JList();
+		IstAngemeldetBenutzer.setBounds(6, 6, 150, 299);
+		contentPane.add(IstAngemeldetBenutzer);
 		
-		JTextArea txtTextnachrichten = new JTextArea();
-		txtTextnachrichten.setBounds(191, 6, 257, 190);
+		txtTextnachrichten = new JTextArea();
+		txtTextnachrichten.setBounds(168, 6, 348, 174);
 		contentPane.add(txtTextnachrichten);
 		
-		JLabel lblAn = new JLabel("an:");
-		lblAn.setBounds(191, 208, 19, 16);
+		lblAn = new JLabel("an:");
+		lblAn.setBounds(168, 190, 61, 16);
 		contentPane.add(lblAn);
 		
-		JTextArea txtEingabeTextnachricht = new JTextArea();
-		txtEingabeTextnachricht.setBounds(191, 232, 257, 82);
+		txtEingabeTextnachricht = new JTextArea();
+		txtEingabeTextnachricht.setBounds(168, 215, 348, 90);
 		contentPane.add(txtEingabeTextnachricht);
 		
-		JButton btnAnmelden = new JButton("anmelden");
-		btnAnmelden.setBounds(0, 326, 105, 29);
+		btnAnmelden = new JButton("anmelden");
+		btnAnmelden.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				geklicktAnmelden();
+			}
+		});
+		btnAnmelden.setBounds(6, 317, 117, 29);
 		contentPane.add(btnAnmelden);
 		
-		JButton btnAbmelden = new JButton("abmelden");
-		btnAbmelden.setEnabled(false);
-		btnAbmelden.setBounds(105, 326, 117, 29);
+		btnAbmelden = new JButton("abmelden");
+		btnAbmelden.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnAbmelden.setBounds(132, 317, 117, 29);
 		contentPane.add(btnAbmelden);
 		
-		JButton btnSenden = new JButton("senden");
-		btnSenden.setEnabled(false);
-		btnSenden.setBounds(224, 326, 117, 29);
+		btnSenden = new JButton("senden");
+		btnSenden.setBounds(261, 317, 117, 29);
 		contentPane.add(btnSenden);
 		
-		JButton btnLoeschen = new JButton("löschen");
-		btnLoeschen.setEnabled(false);
-		btnLoeschen.setBounds(337, 326, 117, 29);
+		btnLoeschen = new JButton("löschen");
+		btnLoeschen.setBounds(399, 317, 117, 29);
 		contentPane.add(btnLoeschen);
 		
 		txtEmpfaenger = new JTextField();
-		txtEmpfaenger.setBackground(new Color(238, 239, 238));
-		txtEmpfaenger.setEnabled(false);
-		txtEmpfaenger.setEditable(false);
-		txtEmpfaenger.setBounds(211, 203, 130, 26);
+		txtEmpfaenger.setBounds(196, 185, 320, 26);
 		contentPane.add(txtEmpfaenger);
 		txtEmpfaenger.setColumns(10);
+	}
+	
+	private void geklicktAnmelden() 
+	{
+		LoginFenster dasLoginFenster = new LoginFenster();
+		dasLoginFenster.setVisible(true);
 	}
 }
