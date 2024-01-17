a824ec510a382451d7d9a49d4bb35420fb7b56c1
Leo <80842710+szxl777@users.noreply.github.com>
2024-01-17 15:00:27 +0100

    Update Steuerung.java

--- TratschClient/src/steuerung/Steuerung.java
+++ TratschClient/src/steuerung/Steuerung.java
@@ -5,19 +5,48 @@ import netzwerk.Netzwerk;
 
 import java.io.IOException;
 
-public class Steuerung 
-{
-	private Benutzerschnittstelle dieBenutzerschnittstelle;
-	private Netzwerk dasNetzwerk;
+public class Steuerung {
 	
-	public Steuerung(Benutzerschnittstelle pBenutzerschnittstelle) throws IOException {
+	private Benutzerschnittstelle dieBenutzerschnittstelle;
+	private Netzwerk dasNetzwerk = new Netzwerk(this);
+	
+	public Steuerung(Benutzerschnittstelle pBenutzerschnittstelle) {
 		dieBenutzerschnittstelle = pBenutzerschnittstelle;
-		dasNetzwerk = new Netzwerk(this);
 	}
 
-	public void geklicktAbmelden()
-	{
+	public void erfolgreichAbgemeldet() {
+		
+	}
+	
+	public void erfolgreichAngemeldet(String pBenutzename) {
+		
+	}
+	
+	public void erhaltenTextnachricht(String pAbsender, String pEmpfaenger, String pTextnachricht) {
+		dieBenutzerschnittstelle.zeigeTextnachricht(pAbsender, pEmpfaenger, pTextnachricht);
+	}
+	
+	public void geklicktAbmelden() {
 		dasNetzwerk.meldeAb();
 	}
-
+	
+	public void geklicktAnmelden(String pBenutzername, String pPassword) {
+		
+	}
+	
+	public boolean istAngemeldet() {
+		
+	}
+	
+	public void sendeTextnachricht(String[] pEmpfaenger, String pTextnachricht) {
+		dasNetzwerk.sendeTextnachricht(pEmpfaenger, pTextnachricht);
+	}
+	
+	public void zeigeAngemeldeteBenutzer(String[] pAngemeldeteBenutzer) {
+		
+	}
+	
+	public void zeigeMeldung(String pMeldung) {
+		dieBenutzerschnittstelle.zeigeMeldung(pMeldung);
+	}
 }
